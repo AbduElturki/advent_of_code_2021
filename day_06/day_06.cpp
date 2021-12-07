b577f19d8a438e61f7fa1ae003defa7e1db30d7c
Abdu <abdussalam@elturki.com>
2021-12-07 10:12:49 +0000

    better day_06 code

--- day_06/day_06.cpp
+++ day_06/day_06.cpp
@@ -5,39 +5,31 @@
 #include <algorithm>
 #include <numeric>
 
-#include <boost/algorithm/string.hpp>
-#include <boost/lexical_cast.hpp>
 
 uint64_t simulate(std::vector<uint64_t> timers, int days)
 {
     while (days-- > 0)
     {
         uint64_t reproduce = timers[0];
-        for (int i = 0; i < timers.size() - 1; ++i)
-            timers[i] = timers[i + 1];
+        std::rotate(timers.begin(), timers.begin() + 1, timers.end());
 
         timers[6] += reproduce;
-        timers[8] = reproduce;
     }
 
-    return std::accumulate(timers.begin(), timers.end(), uint64_t());
+    return std::accumulate(timers.begin(), timers.end(), 0ULL);
 }
 
 int main()
 {
     std::ifstream input("input");
-    std::string s;
-    std::vector<std::string> time_str;
 
-    getline(input, s);
+    std::string s;
+    std::vector<uint64_t> fish_time(9, 0);
+
+    while(getline(input, s, ','))
+        ++fish_time[std::stoi(s)];
 
     input.close();
-
-    boost::split(time_str, s, boost::is_any_of(","));
-
-    std::vector<uint64_t> fish_time(9, 0);
-    for (std::string days_left : time_str)
-        ++fish_time[boost::lexical_cast<uint64_t>(days_left)];
 
     std::cout << "Part 1:\t" << simulate(fish_time, 80) << "\nPart 2:\t" << simulate(fish_time, 256) << std::endl;
 }
